82c05e44e74be22677ec84831f82d2d46fe6535d
Korbin <korbin.westerfield.kw@gmail.com>
2023-02-21 15:03:22 -0700

    added nav bar

---
+++ Portfolio/Home.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var hamburger = document.getElementById("links");
+    if (hamburger.style.display === "block") {
+      hamburger.style.display = "none";
+    } else {
+      hamburger.style.display = "block";
+    }
+  }
