347a76fe5c8a042a0838a667a97746dd4b86c67f
Korbin <korbin.westerfield.kw@gmail.com>
2023-02-22 00:07:55 -0700

    added paragraphs to about me page

--- Portfolio/Home.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var hamburger = document.getElementById("links");
-    if (hamburger.style.display === "block") {
-      hamburger.style.display = "none";
-    } else {
-      hamburger.style.display = "block";
-    }
-  }
