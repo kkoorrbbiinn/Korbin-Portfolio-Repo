5349e0874b67c17ac6a43aec75a1f61edccf6c67
kkoorrbbiinn <korbin.westerfield.kw@gmail.com>
2023-05-08 04:14:41 -0700

    redone portfolio

---
+++ Projects/projects.js
@@ -0,0 +1,9 @@
+document.addEventListener('mousemove', parallax);
+function parallax(e){
+    document.querySelectorAll('.parallax').forEach(function(move){
+        var moving_value = move.getAttribute('data-value');
+        var x = (e.clientX * moving_value) / 250;
+        var y = (e.clientY * moving_value) / 250;
+        move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+}
